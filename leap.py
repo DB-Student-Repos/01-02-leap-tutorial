e67be42b6f35b2b9019e44b806b8e16df6390f66
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-08 09:15:59 +0100

    Update leap.py

--- leap.py
+++ leap.py
@@ -11,7 +11,7 @@ def leap_year(year):
                 return False
             
 def leap_year(year):
-    if(year%4==0) and ((year%100!=0 and year%400==0)):
+    if(year%4==0) and ((year%100!=0 or year%400==0)):
         return True
     else:
         return False
